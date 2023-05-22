706e82e883d06a70aaab651ab418fae615fdd9c8
mariofredo <54829954+mariofredo@users.noreply.github.com>
2023-05-22 08:48:53 +0700

    modify logo in navbar responsive mobile

--- edu.dot/src/components/Navbar.js
+++ edu.dot/src/components/Navbar.js
@@ -181,11 +181,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <span className="clr-orange font-bold text-2xl">Edu.dot</span>
             </a>
             <button
               type="button"
